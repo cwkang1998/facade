9642506f7ff05b24344506d5d48bb1fdf68596ac
chen <23054115+cwkang1998@users.noreply.github.com>
2023-03-04 10:57:14 +0800

    chore: add ecdsa_js for frontend

--- packages/truffle/contracts/ZkMultisigWallet.sol
+++ packages/truffle/contracts/ZkMultisigWallet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.17;
 
-contract SimpleStorage {
+contract ZKMultisigWallet {
   uint256 value;
 
   function read() public view returns (uint256) {
