5c43a7e774759d7f27ce22a3521a228d082265ec
chen <23054115+cwkang1998@users.noreply.github.com>
2023-03-05 21:15:25 +0800

    feat: Complete success view

--- packages/create-site/src/components/Navbar.tsx
+++ packages/create-site/src/components/Navbar.tsx
@@ -2,26 +2,30 @@ import {
   Box,
   Flex,
   Button,
-  useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
-  Heading,
   Text,
+  Link,
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { CardViewState, useCardViewContext } from '../hooks/CardViewContext';
 
 export const Navbar = () => {
+  const [currentView, setCurrentView] = useCardViewContext();
   const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={20} alignItems={'center'} justifyContent={'space-between'}>
           <Box>
-            <Text fontSize='2xl'>Facade</Text>
-          </Box> 
+            <Text fontSize="2xl">
+              <Link onClick={() => setCurrentView(CardViewState.INITIAL)} _hover={{}}>
+                Facade
+              </Link>
+            </Text>
+          </Box>
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
--- packages/create-site/src/views/CreationSuccessful.tsx
+++ packages/create-site/src/views/CreationSuccessful.tsx
@@ -1,16 +1,26 @@
-import { Card, CardBody, CardHeader, Progress, Text } from '@chakra-ui/react';
+import { Card, CardBody, Box, Text, CardFooter } from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
 import { AddSnapButton } from '../components/AddSnapButton';
 
 export const CreationSuccessful = () => {
   return (
     <Card>
-      <CardHeader>
-        <Text>Create your first Zk Multisig Wallet</Text>
-        <Progress value={80} />
-      </CardHeader>
       <CardBody>
-        <AddSnapButton onConnectClick={() => {}} state={{ isFlask: true }} />
+        <Box textAlign="center" py={10} px={6}>
+          <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
+          <Text fontSize="4xl" mt={6} mb={2}>
+            Successfully created your Facade wallet!
+          </Text>
+          <Text color={'gray.500'}>
+            Start using your Facade wallet by adding the snap to Metamask Flask.
+          </Text>
+        </Box>
       </CardBody>
+      <CardFooter>
+        <Box marginRight="auto" marginLeft="auto">
+          <AddSnapButton onConnectClick={() => {}} state={{ isFlask: true }} />
+        </Box>
+      </CardFooter>
     </Card>
   );
 };
