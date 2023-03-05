5f658b19b38237c7abe07bbca480b4176de7045c
cheechyuanang <angchee@live.com.my>
2023-03-05 01:44:13 -0700

    integrate execute proof

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,25 +1,46 @@
 import bodyParser from 'body-parser';
 import express, { Application, Request, Response } from 'express';
 import format from 'pg-format';
-import { Contract, ethers, Wallet } from 'ethers';
 import { getClient, getTestQuery } from './client';
 import zkWalletAbi from './abi/zkWalletAbi.json';
+import { ethers, Wallet } from 'ethers';
 
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
 const privateKey = process.env.PRIVATE_KEY!;
 const infuraKey = process.env.INFURA_KEY!;
-
 const rpcUrl = `https://goerli.infura.io/v3/${infuraKey}`;
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 const wallet = new Wallet(privateKey, provider);
 
 const zkWalletInterface = new ethers.utils.Interface(zkWalletAbi);
 
-const sendExecute = async (zkWalletAddr: string, proofs: string[]) => {
-  const inputData = zkWalletInterface.encodeFunctionData('execute', [proofs]);
+const sendExecute = async (
+  zkWalletAddr: string,
+  proofs: Proof[],
+  functionInputData: string,
+) => {
+  const a_s = [];
+  const b_s = [];
+  const c_s = [];
+  const inputs = [];
+
+  for (let i = 0; i < proofs.length; i++) {
+    a_s.push(proofs[i].a);
+    b_s.push(proofs[i].b);
+    c_s.push(proofs[i].c);
+    inputs.push(proofs[i].input);
+  }
+  const inputData = zkWalletInterface.encodeFunctionData('execute', [
+    a_s,
+    b_s,
+    c_s,
+    inputs,
+    zkWalletAddr,
+    functionInputData,
+  ]);
+
   const tx = await wallet.sendTransaction({
     to: zkWalletAddr,
     data: inputData,
@@ -124,14 +145,23 @@ app.delete('/id/:id', async (req: Request, res: Response) => {
   return;
 });
 
+interface Proof {
+  a: any;
+  b: any;
+  c: any;
+  input: any;
+}
 app.post('/relay', async (req: Request, res: Response): Promise<void> => {
   const data = req.body;
 
   const walletAddress = data.walletAddress;
-  const proofs: string[] = data.proofs;
+  const proofs: Proof[] = data.proofs;
   const network = data.network;
 
-  const tx = await sendExecute(walletAddress, proofs);
+  const xx = new ethers.utils.Interface(zkWalletAbi);
+  const inputData = zkWalletInterface.encodeFunctionData('nonce');
+
+  const tx = await sendExecute(walletAddress, proofs, inputData);
 
   res.status(200);
   res.json(tx);
